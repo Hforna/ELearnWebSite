5d2ffa8bfb30eea7c037ab3a80e870ff5bf29b1f
Hforna <henriqueflashpingo@gmail.com>
2025-01-08 18:35:56 -0300

    create background service for delete user after 30 days

--- src/backend/user/User.Api/BackgroundServices/DeleteAccountService.cs
+++ src/backend/user/User.Api/BackgroundServices/DeleteAccountService.cs
@@ -1,5 +1,55 @@
-﻿
+﻿using User.Api.Models.Repositories;
+
 namespace User.Api.BackgroundServices
 {
+    public class DeleteAccountService : BackgroundService
+    {
+        private readonly ILogger<DeleteAccountService> _logger;
+        private readonly IServiceProvider _serviceProvider;
 
+        public DeleteAccountService(ILogger<DeleteAccountService> logger, IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("DeleteAccountService iniciado em {Time}.", DateTime.UtcNow);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using (var scope = _serviceProvider.CreateScope())
+                    {
+                        var uof = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+                        var users = await uof.userReadOnly.GetUsersNotActive();
+                        if (users == null || !users.Any())
+                        {
+                            _logger.LogInformation("None user inactivate was found");
+                        }
+                        else
+                        {
+                            foreach (var user in users)
+                            {
+                                if (user.TimeDisabled <= DateTime.UtcNow)
+                                {
+                                    uof.userWriteOnly.DeleteUser(user);
+                                    await uof.Commit();
+                                    _logger.LogInformation("User {UserId} deleted", user.Id);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error while executing the DeleteAccountService service.");
+                }
+                await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+            }
+        }
+    }
 }
