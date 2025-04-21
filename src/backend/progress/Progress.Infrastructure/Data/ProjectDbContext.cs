a890c46415bcdc2e286620fda8318942afa48ef5
Hforna <henriqueflashpingo@gmail.com>
2025-04-21 17:57:56 -0300

    build configure progress service on docker compose

---
+++ src/backend/progress/Progress.Infrastructure/Data/ProjectDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Progress.Infrastructure.Data
+{
+    public class ProjectDbContext : DbContext
+    {
+        protected ProjectDbContext(DbContextOptions<ProjectDbContext> dbContext) : base(dbContext)
+        {
+        }
+
+        //public DbSet<Certificate> Certificates { get; set; }
+        //public DbSet<>
+        //
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //
+        //    modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProjectDbContext).Assembly);
+        //}
+    }
+}
