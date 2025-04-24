ff50149a5a52efbe1b4143d648cb5fe98c814788
Hforna <henriqueflashpingo@gmail.com>
2025-04-24 18:42:09 -0300

    feat create culture middlware on progress service

---
+++ src/backend/progress/Progress.Api/Middlewares/CultureInfoMiddleware.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Globalization;
+
+namespace Progress.Api.Middlewares
+{
+    public class CultureInfoMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var requestLanguage = context.Request.Headers.AcceptLanguage;
+            var acceptedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            var currentCulture = new CultureInfo("en-US");
+
+            if(string.IsNullOrEmpty(requestLanguage) == false && acceptedLanguages.Any(d => d.Equals(acceptedLanguages)))
+            {
+                currentCulture = new CultureInfo(requestLanguage!);
+            }
+
+            CultureInfo.CurrentUICulture = currentCulture;
+            CultureInfo.CurrentCulture = currentCulture;
+
+            await next(context);
+        }
+    }
+}
--- src/backend/progress/Progress.Api/Program.cs
+++ src/backend/progress/Progress.Api/Program.cs
@@ -1,3 +1,4 @@
+using Progress.Api.Middlewares;
 using Progress.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<CultureInfoMiddleware>();
 
 app.UseAuthorization();
 
